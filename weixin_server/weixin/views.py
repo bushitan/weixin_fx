61b66fcd19a9ab1202b25a7bbf429fa848058ddc
bushitan <373514952@qq.com>
2016-08-16 16:19:20 +0800

    v 1.2 show字符画 ，精装修

--- weixin_server/weixin/views.py
+++ weixin_server/weixin/views.py
@@ -168,7 +168,9 @@ def AutoReplyService(request):
         _str_url = _res_json['str_url'] #字符画地址
         _img_url = _res_json['img_url']  #原图地址
         #微信跳转链接
-        _paw_url = "http://bushitan.pythonanywhere.com/art/show/?url=" + _str_url
+        # _paw_url = "http://bushitan.pythonanywhere.com/art/show/?url=" + _str_url   #pythonanywhere 的链接
+        _paw_url = "http://120.27.97.33:82/blog/artwork/show/?url="+_str_url +"&open_id=" + context['to_user_name'] #阿里云的链接 需要图片名称，用户open_id
+
 
         #添加游戏数据
         game_add_url = SETTING.GAME_ADD
