6fb9999f7af99074b590373caf9da56a76152692
bushitan <373514952@qq.com>
2016-08-25 19:14:13 +0800

    v 1.5.3 sketch 少个=

--- weixin_server/weixin/views.py
+++ weixin_server/weixin/views.py
@@ -220,7 +220,7 @@ def AutoReplyService(request):
         _sketch_url = _res_json['sketch_url']  #原图地址
         #微信跳转链接
         # _paw_url = "http://bushitan.pythonanywhere.com/art/show/?url=" + _str_url   #pythonanywhere 的链接
-        _paw_url = "http://120.27.97.33:82/blog/artwork/show/?str_img="+_str_url + "&origin_img="+ _img_url + "&sketch_img" + _sketch_url +"&open_id=" + context['to_user_name'] #阿里云的链接 需要图片名称，用户open_id
+        _paw_url = "http://120.27.97.33:82/blog/artwork/show/?str_img="+_str_url + "&origin_img="+ _img_url + "&sketch_img=" + _sketch_url +"&open_id=" + context['to_user_name'] #阿里云的链接 需要图片名称，用户open_id
 
 
         #添加游戏数据
